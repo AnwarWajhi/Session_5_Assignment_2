e6364a3d59cd0841963725c0f9af79d04ac0b63b
AnwarWajhi <anwarwajhi5@gmail.com>
2018-02-23 23:40:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var myObject = {
+    name: 'Wajhi',
+    age: 26,
+    dateofbirth: '05-08-1991',
+    placeofbirth:"Hyderabad"
+};
+
+//Printing Object in console
+console.log("Name: "+myObject.name);
+console.log("Age: "+myObject.age);
+console.log("DOB: "+myObject.dateofbirth);
+console.log("Place of Birth: "+myObject.placeofbirth);
